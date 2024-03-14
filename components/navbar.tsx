43dc8d4bc3ff0efa32205714e784f1fa179e7b97
gualberto2 <elijahwilsonnn@gmail.com>
2024-03-14 09:29:57 -0700

    theme toggle, profile button

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,6 @@
 import { Navigation } from "./navigation-menu";
+import { ThemeToggle } from "./theme-toggle";
+import { Profile } from "./ui/user-profile";
 
 const Navbar = async () => {
   return (
@@ -6,10 +8,9 @@ const Navbar = async () => {
       <div className="border-b">
         <div className="flex h-16 items-center px-4">
           <Navigation className="mx-6" />
-          {/* <div className="ml-auto flex items-center space-x-4">
-            <ThemeToggle />
-            <UserButton afterSignOutUrl="/" />
-          </div> */}
+          <div className="ml-auto flex items-center space-x-4">
+            <Profile /> <ThemeToggle />
+          </div>
         </div>
       </div>
     </div>
