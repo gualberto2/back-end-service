e36d10c43537d9c3de622aab83369e14ce0cb936
gualberto2 <elijahwilsonnn@gmail.com>
2024-03-14 09:30:32 -0700

    user profile and data

--- components/ui/user-profile.tsx
+++ components/ui/user-profile.tsx
@@ -3,37 +3,43 @@
 import { createClient } from "@/lib/supabase/client";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+
+import auth from "@/context/get-user";
+
 import { toast } from "./use-toast";
 
+import { CgProfile } from "react-icons/cg";
+
+import {
+  Cloud,
+  CreditCard,
+  Github,
+  Keyboard,
+  LifeBuoy,
+  LogOut,
+  Mail,
+  MessageSquare,
+  Plus,
+  PlusCircle,
+  Settings,
+  User,
+  UserPlus,
+  Users,
+} from "lucide-react";
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
+  DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuLabel,
+  DropdownMenuPortal,
   DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import auth from "@/context/get-user";
-
 export function Profile() {
-  const [loading, setLoading] = useState(false);
-
-  const router = useRouter();
-
-  const supabase = createClient();
-  const user = auth();
-
-  const handleLogout = async () => {
-    try {
-      setLoading(true);
-      const { error } = await supabase.auth.signOut();
-
-      if (!error) {
-        router.push("/");
-      }
-
-      toast({ title: "Successfully logged out" });
-
-      router.refresh();
-      setLoading(false);
-    } 
+  const [
