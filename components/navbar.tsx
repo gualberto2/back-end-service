62973d4cf08118f3dcea527dd601037f538b5b7f
gualberto2 <elijahwilsonnn@gmail.com>
2024-03-14 09:35:36 -0700

    style: rounded full

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = async () => {
   return (
     <div className="border-b">
       <div className="border-b">
-        <div className="flex h-16 items-center px-4">
+        <div className="flex h-14 items-center px-4">
           <Navigation className="mx-6" />
           <div className="ml-auto flex items-center space-x-4">
             <Profile /> <ThemeToggle />
