e9657012ea5a9f58a1d5e341945370fd63ed62f2
gualberto2 <elijahwilsonnn@gmail.com>
2024-03-14 07:43:05 -0700

    Form to create bes entity

---
+++ components/bes-create.tsx
@@ -0,0 +1,99 @@
+"use client";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Button } from "./ui/button";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { z } from "zod";
+import axios from "axios";
+import { Input } from "./ui/input";
+import { createClient } from "@/lib/supabase/client";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
+
+const CreateBes = () => {
+  const [loading, setLoading] = useState(false);
+
+  const supabase = createClient();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setLoading(true);
+
+      const { data, error } = await supabase
+        .from("bes")
+        .insert([{ name: values.name }])
+        .select();
+    } catch (error) {
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <Dialog>
+      <DialogTrigger>Create BES</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create a BES Here</DialogTitle>
+          <DialogDescription>Please provide a name</DialogDescription>
+          <div className="">
+            <div className="space-y-4 py-2 pb-4">
+              <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                  <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Name</FormLabel>
+                        <FormControl>
+                          <Input
+                            disabled={loading}
+                            placeholder="Ecommerce"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                    <Button variant="outline">Cancel</Button>
+                    <Button disabled={loading} type="submit">
+                      Continue
+                    </Button>
+                  </div>
+                </form>
+              </Form>
+            </div>
+          </div>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
+export default CreateBes;
