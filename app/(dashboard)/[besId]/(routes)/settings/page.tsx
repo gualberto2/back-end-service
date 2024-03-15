b1348d7bbea7084bc5023d562f28d09389af5498
gualberto2 <elijahwilsonnn@gmail.com>
2024-03-14 19:24:55 -0700

    temp

---
+++ app/(dashboard)/[besId]/(routes)/settings/page.tsx
@@ -0,0 +1,17 @@
+import { redirect } from "next/navigation";
+
+import { SettingsForm } from "./components/settings-form";
+
+const SettingsPage = async ({ params }: { params: { storeId: string } }) => {
+  //Check if store even exists, not: redirect to home page
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SettingsForm />
+      </div>
+    </div>
+  );
+};
+
+export default SettingsPage;
