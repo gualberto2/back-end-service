f0b251b6d31189c3f1f9a9f2513ae37b9bac6eeb
gualberto2 <elijahwilsonnn@gmail.com>
2024-03-22 16:41:59 -0700

    author page turned into form

--- app/(dashboard)/[besId]/(routes)/blog/author/add/page.tsx
+++ app/(dashboard)/[besId]/(routes)/blog/author/add/page.tsx
@@ -1,8 +1,139 @@
-const AddAuthor = () => {
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Separator } from "@/components/ui/separator";
+import { Heading } from "@/components/ui/heading";
+import { createClient } from "@/lib/supabase/client";
+import { AuthorColumn } from "../components/columns";
+import { toast } from "@/components/ui/use-toast";
+
+const formSchema = z.object({
+  name: z.string().min(2),
+});
+
+const AddAuthor = ({ params }: { params: { besId: string } }) => {
+  const router = useRouter();
+
+  const supabase = createClient();
+
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setLoading(true);
+
+      const { data, error } = await supabase
+        .from("author")
+        .insert([{ name: values.name, blog_id: params.besId }])
+        .select();
+
+      if (error) {
+        console.log("no work", error);
+        throw error;
+      }
+
+      console.log(values, "HERE DA DATA");
+
+      router.refresh();
+      router.push(`/${params.besId}/blog/author`);
+      toast({ title: "success" });
+    } catch (error: any) {
+      toast({ title: "Something went wrong.", variant: "destructive" });
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <form>
-      <div>ADD AUTHOR</div>
-    </form>
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <div className="flex items-center justify-between">
+          <Heading title={"Create Author"} description={"Create author here"} />
+        </div>
+        <Separator />
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 w-full"
+          >
+            <div className="md:grid md:grid-cols-3 gap-8">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={loading}
+                        placeholder="Author name"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* <FormField
+              control={form.control}
+              name="billboardId"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Billboard</FormLabel>
+                  <Select
+                    disabled={loading}
+                    onValueChange={field.onChange}
+                    value={field.value}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue
+                          defaultValue={field.value}
+                          placeholder="Select a billboard"
+                        />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {billboards.map((billboard) => (
+                        <SelectItem key={billboard.id} value={billboard.id}>
+                          {billboard.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            /> */}
+            </div>
+            <Button disabled={loading} className="ml-auto" type="submit">
+              Create
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </div>
   );
 };
+
 export default AddAuthor;
