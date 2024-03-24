3aa9c4bf6ede6bd44ee834f4175363ff673c4081
gualberto2 <elijahwilsonnn@gmail.com>
2024-03-23 20:04:44 -0700

    feat: Post form

---
+++ app/(dashboard)/[besId]/(routes)/blog/posts/[postId]/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { createClient } from "@/lib/supabase/client";
+import { PostsForm } from "./components/post-form";
+import { useEffect, useState } from "react";
+import { PostsColumn } from "../components/columns";
+
+const AuthorPage = ({ params }: { params: { besId: string } }) => {
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState<PostsColumn | null>(null);
+  const supabase = createClient();
+
+  useEffect(() => {
+    const getAuthor = async () => {
+      const { data, error } = await supabase
+        .from("posts")
+        .select("*")
+        .eq("blog_id", params.besId) // Assuming besId is the id of the author you want to edit
+        .single(); // Using .single() since we expect only one record
+
+      console.log(data);
+
+      setLoading(false);
+      if (error) {
+        console.error("Error fetching data:", error);
+      } else if (data) {
+        setPosts(data);
+      }
+    };
+
+    getAuthor();
+  }, [params.besId, supabase]);
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6 w-full">
+        <PostsForm initialData={posts} />
+      </div>
+    </div>
+  );
+};
+
+export default AuthorPage;
