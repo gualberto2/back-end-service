9cc2bb69714008e2b611ca35f879a301315777ef
gualberto2 <elijahwilsonnn@gmail.com>
2024-03-23 20:06:56 -0700

    settings page

--- app/(dashboard)/[besId]/(routes)/blog/settings/page.tsx
+++ app/(dashboard)/[besId]/(routes)/blog/settings/page.tsx
@@ -1,39 +1,40 @@
 "use client";
 
 import { createClient } from "@/lib/supabase/client";
-import { redirect } from "next/navigation";
 import { useEffect, useState } from "react";
-import { Bes, SettingsForm } from "./components/settings-form";
+import { SettingsForm } from "./components/settings-form";
+import { Settings } from "@/utils/types";
 
 const SettingsPage = ({ params }: { params: { besId: string } }) => {
-  const [bes, setBes] = useState<Bes | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [settings, setSettings] = useState<Settings | null>(null);
   const supabase = createClient();
-  const fetchData = async () => {
-    try {
-      let { data: besData, error } = await supabase
-        .from("bes")
-        .select("*")
-        .eq("id", params.besId)
-        .single(); // Assuming you're fetching a single record based on `besId`
-
-      if (error) {
-        console.error("Error fetching data:", error);
-      } else {
-        setBes(besData); // Set the fetched data directly
-      }
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
 
   useEffect(() => {
-    fetchData();
-  }, [params.besId]); // The empty array ensures this effect runs only once after initial render
+    const getAuthor = async () => {
+      const { data, error } = await supabase
+        .from("blog")
+        .select("*")
+        .eq("id", params.besId)
+        .single();
+
+      console.log(data);
+
+      setLoading(false);
+      if (error) {
+        console.error("Error fetching data:", error);
+      } else if (data) {
+        setSettings(data);
+      }
+    };
+
+    getAuthor();
+  }, [params.besId, supabase]);
 
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        {bes && <SettingsForm initialData={bes} />}
+        <SettingsForm initialData={settings} />
       </div>
     </div>
   );
