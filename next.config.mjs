215ce2dc642029c1654c626ff38006aeca19b2e5
gualberto2 <elijahwilsonnn@gmail.com>
2024-03-25 12:43:19 -0700

    iamge url

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    remotePatterns: [
+      {
+        protocol: "https", // or 'http' depending on your requirement
+        hostname: "kzgqkcieuscdyzgnlvvk.supabase.co",
+        // Optionally include pathname if you want to match specific paths
+        // pathname: '/storage/v1/object/public/post-images/*',
+      },
+    ],
+    domains: ["kzgqkcieuscdyzgnlvvk.supabase.co"],
     domains: ["pbs.twimg.com"], // Add the external domain here
   },
 };
