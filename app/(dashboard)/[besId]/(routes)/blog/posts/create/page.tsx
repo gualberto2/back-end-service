87bdec69a8a06999a7e4895b12221c11dcab5dce
gualberto2 <elijahwilsonnn@gmail.com>
2024-03-25 18:47:15 -0700

    removed quill, added form  props for posts

--- app/(dashboard)/[besId]/(routes)/blog/posts/create/page.tsx
+++ app/(dashboard)/[besId]/(routes)/blog/posts/create/page.tsx
@@ -6,9 +6,6 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
 
-import "quill/dist/quill.core.css";
-import Quill from "quill";
-
 import { createClient } from "@/lib/supabase/client";
 
 import { Input } from "@/components/ui/input";
@@ -16,6 +13,7 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -26,20 +24,25 @@ import { Separator } from "@/components/ui/separator";
 import { Heading } from "@/components/ui/heading";
 import { toast } from "@/components/ui/use-toast";
 import { Textarea } from "@/components/ui/textarea";
+import Tiptap from "@/components/tiptap";
+import ImageUpload from "@/components/ui/image-upload";
+import { Checkbox } from "@/components/ui/checkbox";
 
 const formSchema = z.object({
   title: z.string().min(2),
   body: z.string().min(2),
   hook: z.string().min(2),
   conclusion: z.string().min(2),
-  code: z.string().min(2),
-  images: z.object({ url: z.string() }).array(),
-  isFeatured: z.boolean().default(false).optional(),
+  // code: z.string().min(2),
+
+  is_featured: z.boolean().default(false).optional(),
+  is_archived: z.boolean().default(false).optional(),
 });
 
 const AddPost = ({ params }: { params: { besId: string } }) => {
+  const [imageUrls, setImageUrls] = useState([]);
+
   const router = useRouter();
-  const quillRef = useRef(null); // Ref to attach Quill
 
   const supabase = createClient();
 
@@ -49,23 +52,9 @@ const AddPost = ({ params }: { params: { besId: string } }) => {
     resolver: zodResolver(formSchema),
   });
 
-  useEffect(() => {
-    if (quillRef.current) {
-      const quill = new Quill(quillRef.current, {
-        modules: {
-          toolbar: [
-            ["bold", "italic", "underline", "strike"],
-            [{ list: "ordered" }, { list: "bullet" }],
-            ["link", "image"],
-          ],
-        },
-      });
-
-      quill.on("text-change", () => {
-        form.setValue("body", quill.root.innerHTML);
-      });
-    }
-  }, []);
+  // const handleImageUpload = (newUrl: string) => {
+  //   setImageUrls((prevUrls) => [...prevUrls, newUrl]);
+  // };
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
@@ -78,9 +67,10 @@ const AddPost = ({ params }: { params: { besId: string } }) => {
             title: values.title,
             body: values.body,
             hook: values.hook,
-            code: values.code,
-            images: values.images,
-            isFeatured: values.isFeatured,
+            // code: values.code,
+
+            is_featured: values.is_featured,
+            is_archived: values.is_archived,
 
             blog_id: params.besId,
           },
@@ -92,10 +82,8 @@ const AddPost = ({ params }: { params: { besId: string } }) => {
         throw error;
       }
 
-      console.log(values, "HERE DA DATA");
-
       router.refresh();
-      router.push(`/${params.besId}/blog/author`);
+      router.push(`/${params.besId}/blog/posts`);
       toast({ title: "success" });
     } catch (error: any) {
       toast({ title: "Something went wrong.", variant: "destructive" });
@@ -108,15 +96,25 @@ const AddPost = ({ params }: { params: { besId: string } }) => {
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-2 md:p-5 pt-10">
         <div className="flex items-center justify-between">
-          <Heading title={"Create Author"} description={"Create author here"} />
+          <Heading
+            title={"Create Post"}
+            description={
+              "Create post here, please include a title, body content, hook, images, and author."
+            }
+          />
         </div>
         <Separator />
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
-            className="space-y-8 w-full"
+            className="space-y-2 md:space-y-6 w-full"
           >
-            <div className="flex flex-col gap-8">
+            <div className="flex flex-col gap-6 md:gap-8">
+              {/* <ImageUpload
+                bucketName="post-images"
+                onUpload={handleImageUpload}
+              /> */}
+
               <FormField
                 control={form.control}
                 name="title"
@@ -126,7 +124,7 @@ const AddPost = ({ params }: { params: { besId: string } }) => {
                     <FormControl>
                       <Input
                         disabled={loading}
-                        placeholder="Author name"
+                        placeholder="Provide a title for the post"
                         {...field}
                       />
                     </FormControl>
@@ -151,22 +149,81 @@ const AddPost = ({ params }: { params: { besId: string } }) => {
                   </FormItem>
                 )}
               />
+
               <FormField
                 control={form.control}
                 name="body"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel>body</FormLabel>
+                    <FormLabel>Body</FormLabel>
                     <FormControl>
-                      <div
-                        ref={quillRef}
-                        className="quill-editor border border-neutral-200 dark:text-neutral-300 text-neutral-900"
+                      <Tiptap body={field.value} onChange={field.onChange} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="conclusion"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Conclusion</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={loading}
+                        placeholder="Conclusion here will be shown at end of post."
+                        {...field}
                       />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
                 )}
               />
+              <div className="flex flex-row gap-2">
+                <FormField
+                  control={form.control}
+                  name="is_featured"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
+                      <FormControl>
+                        <Checkbox
+                          checked={field.value}
+                          // @ts-ignore
+                          onCheckedChange={field.onChange}
+                        />
+                      </FormControl>
+                      <div className="space-y-1 leading-none">
+                        <FormLabel>Featured</FormLabel>
+                        <FormDescription>
+                          This post will appear on the home page (Optional)
+                        </FormDescription>
+                      </div>
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="is_archived"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
+                      <FormControl>
+                        <Checkbox
+                          checked={field.value}
+                          // @ts-ignore
+                          onCheckedChange={field.onChange}
+                        />
+                      </FormControl>
+                      <div className="space-y-1 leading-none">
+                        <FormLabel>Archived</FormLabel>
+                        <FormDescription>
+                          This post will dissapear on the home page (Optional)
+                        </FormDescription>
+                      </div>
+                    </FormItem>
+                  )}
+                />
+              </div>
             </div>
             <Button disabled={loading} className="ml-auto" type="submit">
               Create
