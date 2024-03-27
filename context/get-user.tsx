50b0c73e1d41c50f85d3129f37dc9ef21f57497c
gualberto2 <elijahwilsonnn@gmail.com>
2024-03-27 08:07:00 -0700

    returns and logs userid

--- context/get-user.tsx
+++ context/get-user.tsx
@@ -6,7 +6,15 @@ const auth = async () => {
     data: { user },
   } = await supabase.auth.getUser();
 
-  return user;
+  // Check if user is not null before trying to access its properties
+  if (user) {
+    console.log(user.id);
+    return user;
+  } else {
+    // Handle the case where user is null, for example, by returning null or throwing an error
+    console.log("User not found");
+    return null;
+  }
 };
 
 export default auth;
