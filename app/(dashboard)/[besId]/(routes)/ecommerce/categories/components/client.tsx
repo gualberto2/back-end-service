6025d330d9815930a8b4314d53fdc2d685e07c08
gualberto2 <elijahwilsonnn@gmail.com>
2024-04-01 16:57:32 -0700

    category action fix

--- app/(dashboard)/[besId]/(routes)/ecommerce/categories/components/client.tsx
+++ app/(dashboard)/[besId]/(routes)/ecommerce/categories/components/client.tsx
@@ -22,11 +22,11 @@ export const CategoriesClient: React.FC<CategoryClientProps> = ({ data }) => {
       <div className="flex items-center justify-between">
         <Heading
           title={`Categories (${data.length})`}
-          description="Manage billboards for your store"
+          description="Manage categories for your store"
         />
         <Button
           onClick={() =>
-            router.push(`/${params.besId}/ecommerce/categories/new}`)
+            router.push(`/${params.besId}/ecommerce/categories/new`)
           }
         >
           <Plus className="mr-2 h-4 w-4" />
